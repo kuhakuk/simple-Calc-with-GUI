9a029aa86ededa314ac7d27b4d47c96294ed3d6c
Khaled Walid <92208826+kuhakuk@users.noreply.github.com>
2021-10-13 08:39:34 +0600

    madlibs

---
+++ main2.py
@@ -0,0 +1,14 @@
+#madlibs
+
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+verb3 = input("verb: ")
+person_name = input("person_name: ")
+
+
+madlib = f"Programming can be {adj}. But it helps us use our mind in all way {verb1}\
+I love to {verb2}. What really got into programming was to build stuffs and help\
+other {verb3}. Stay vigilant like you are {person_name}"
+
+print(madlib)
